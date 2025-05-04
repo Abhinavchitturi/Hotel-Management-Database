6d5b940ebe0c1e4748bb4e1c16c8f9a2ba780ffe
Abhinav <chitturiabhinav1@gmail.com>
2025-05-04 06:30:13 +0530

    Add initial project files

---
+++ Main.js
@@ -0,0 +1,47 @@
+const fname1 = document.getElementById('1-fname');
+const lname1 = document.getElementById('1-lname');
+const sex1 = document.getElementById('1-sex');
+const age1 = document.getElementById('1-age');
+const vtype1 = document.getElementById('1-vtype');
+const vnumber1 = document.getElementById('1-vnumber');
+const phno1 = document.getElementById('1-phno1');
+const phno2 = document.getElementById('1-phno2');
+const button1 = document.getElementById('1-button');
+
+const cid2 = document.getElementById('2-cid');
+const rid2 = document.getElementById('2-rid');
+const rating2 = document.getElementById('2-rating');
+const button2 = document.getElementById('2-button');
+
+const eid3 = document.getElementById('3-eid');
+const button3 = document.getElementById('3-button');
+const amount = document.getElementById('3-amount');
+
+const bid4 = document.getElementById('4-bid');
+const typeroom4 = document.getElementById('4-typeroom');
+const noofguests4 = document.getElementById('4-noofguests');
+const cancellation4 = document.getElementById('4-cancellation');
+const button4 = document.getElementById('4-button');
+const roomno4 = document.getElementById('4-roomno');
+const price4 = document.getElementById('4-price');
+
+const cid5 = document.getElementById('5-cid');
+const button5 = document.getElementById('5-button');
+const bid5 = document.getElementById('5-bid');
+const billdate5 = document.getElementById('5-billdate');
+const amount5 = document.getElementById('5-amount');
+
+const fname6 = document.getElementById('6-fname');
+const lname6 = document.getElementById('6-lname');
+const age6 = document.getElementById('6-age');
+const sex6 = document.getElementById('6-sex');
+const eid6 = document.getElementById('6-eid');
+const rid6 = document.getElementById('6-rid');
+const sal6 = document.getElementById('6-sal');
+const job6 = document.getElementById('6-job');
+const phon16 = document.getElementById('6-phno1');
+const phon26 = document.getElementById('6-phno2');
+const button6 = document.getElementById('6-button');
+
+const eid7 = document.getElementById('7-eid');
+const button7 = document.getElementById('7-button');
